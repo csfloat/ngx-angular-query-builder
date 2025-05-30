f23b2ebcedcfb42342249f796534ec0d9e13ab89
GODrums <armin.stanitzok@outlook.de>
2025-05-30 20:08:26 +0200

    chore: bump to angular 19

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -10,7 +10,8 @@ import {
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
-  styleUrls: ["app.component.scss"]
+  styleUrls: ["app.component.scss"],
+  standalone: false
 })
 export class AppComponent {
   public queryCtrl: UntypedFormControl;
